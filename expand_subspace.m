40d7bf16023abbdd593037df9ca49029237b86a3
gferrandi <g.ferrandi@tue.nl>
2023-11-15 09:44:47 +0100

    replace normalization with qr decomposition

--- expand_subspace.m
+++ expand_subspace.m
@@ -1,7 +1,9 @@
 function [U, jold, j] = expand_subspace(U, j, m2, u)
+
     % Add a vector to the search subspace; make it orthogonal, with norm 1
     u = u - U(:,1:j) * (U(:,1:j)' * u);
-    u = u ./ sqrt(sum(abs(u).^2, 1));
+    [u,~] = qr(u,0);
+    
     jold = j;
     j = min(j+size(u, 2), m2);
     U(:, (jold+1):j) = u(:,1:(j-jold));
