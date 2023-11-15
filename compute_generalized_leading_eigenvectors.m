ac9da5f7c9e5ef9b2002ca5fbb85b15caf68c928
gferrandi <g.ferrandi@tue.nl>
2023-11-15 09:46:06 +0100

    add symmetrization of matrices

--- compute_generalized_leading_eigenvectors.m
+++ compute_generalized_leading_eigenvectors.m
@@ -1,5 +1,6 @@
 function [X, eigenvalues] = compute_generalized_leading_eigenvectors(A, B, j)
-    [X, eigenvalues] = eig(A, B, 'chol');
+    % eig(B)'
+    [X, eigenvalues] = eig(0.5*(A + A'), 0.5*(B + B'), 'chol');
     [eigenvalues, ind] = sort(diag(eigenvalues), 'descend');
     X = X(:, ind(1:j));
     eigenvalues = eigenvalues(1:j);
--- compute_leading_eigenvectors.m
+++ compute_leading_eigenvectors.m
@@ -1,5 +1,5 @@
 function [X, eigenvalues] = compute_leading_eigenvectors(A, j)
-    [X, eigenvalues] = eig(A);
+    [X, eigenvalues] = eig(0.5*(A+A'));
     [eigenvalues, ind] = sort(diag(eigenvalues), 'descend');
     X = X(:, ind(1:j));
     eigenvalues = eigenvalues(1:j);
